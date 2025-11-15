b434c79d71d4443b93612897138495c8e107623d
taro-kayo <40859826+taro-kayo@users.noreply.github.com>
2025-11-15 10:21:12 +0900

    refactor: add __repr__ to Problem

--- statelint/problem/problem.py
+++ statelint/problem/problem.py
@@ -11,3 +11,6 @@ class Problem:
 
     def __str__(self) -> str:
         return self.description
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.description}, {self.type})"
